2b72b1cdf03607d2016cd6c78c41eb21ba11a36e
Yurii Sudarskii <ya.donny@gmail.com>
2019-10-28 15:05:01 +0300

    added container

---
+++ src/components/Card.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { format } from "date-fns"; // from https://date-fns.org
+
+export default function Card({ obj }) {
+  return (
+    <div className="weather-elem col-md-4 col-sm-6">
+      <h2 className="title-widget">
+        {obj.name}, {obj.sys.country}
+      </h2>
+      <h3 className="temp">{obj.main.temp}°C</h3>
+      <p className="weather-p">
+        {obj.weather[0].description[0].toUpperCase() +
+          obj.weather[0].description.slice(1)}
+      </p>
+      <p className="weather-p">{format(obj.dt * 1000, "HH:mm MMM d")}</p>
+      {!obj.firstCard ? (
+        <div>
+          <i
+            className="fa fa-times-circle"
+            onClick={() => this.props.delWeatherWidget(obj.id)}
+          ></i>
+        </div>
+      ) : null}
+      <table className="weather-table">
+        <tbody>
+          <tr>
+            <td className="weather-table__td">Wind</td>
+            <td className="weather-table__td">{obj.wind.speed}m/s</td>
+          </tr>
+          <tr>
+            <td className="weather-table__td">Tempreture (max/min)</td>
+            <td className="weather-table__td">
+              {obj.main.temp_max}°C / {obj.main.temp_min}°C
+            </td>
+          </tr>
+          <tr>
+            <td className="weather-table__td">Pressure</td>
+            <td className="weather-table__td">{obj.main.pressure} hpa</td>
+          </tr>
+          <tr>
+            <td className="weather-table__td">Humidity</td>
+            <td className="weather-table__td">{obj.main.humidity} %</td>
+          </tr>
+          <tr>
+            <td className="weather-table__td">Sunrise</td>
+            <td className="weather-table__td">
+              {format(obj.sys.sunrise * 1000, "HH:mm")}
+            </td>
+          </tr>
+          <tr>
+            <td className="weather-table__td">Sunset</td>
+            <td className="weather-table__td">
+              {format(obj.sys.sunset * 1000, "HH:mm")}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { format } from "date-fns"; // from https://date-fns.org
 import { toDelWidget } from "../redux/actions";
+import Card from "./Card";
 
 class WeatherInfo extends Component {
   render() {
@@ -10,71 +10,7 @@ class WeatherInfo extends Component {
         {!this.props.weatherData[0]
           ? null
           : this.props.weatherData.map(obj =>
-              obj.cod === "404" ? null : (
-                <div key={obj.id} className="weather-elem col-md-4 col-sm-6">
-                  <h2 className="title-widget">
-                    {obj.name}, {obj.sys.country}
-                  </h2>
-                  <h3 className="temp">{obj.main.temp}°C</h3>
-                  <p className="weather-p">
-                    {obj.weather[0].description[0].toUpperCase() +
-                      obj.weather[0].description.slice(1)}
-                  </p>
-                  <p className="weather-p">
-                    {format(obj.dt * 1000, "HH:mm MMM d")}
-                  </p>
-                  {!obj.firstCard ? (
-                    <div>
-                      <i
-                        className="fa fa-times-circle"
-                        onClick={() => this.props.delWeatherWidget(obj.id)}
-                      ></i>
-                    </div>
-                  ) : null}
-                  <table className="weather-table">
-                    <tbody>
-                      <tr>
-                        <td className="weather-table__td">Wind</td>
-                        <td className="weather-table__td">
-                          {obj.wind.speed}m/s
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="weather-table__td">
-                          Tempreture (max/min)
-                        </td>
-                        <td className="weather-table__td">
-                          {obj.main.temp_max}°C / {obj.main.temp_min}°C
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="weather-table__td">Pressure</td>
-                        <td className="weather-table__td">
-                          {obj.main.pressure} hpa
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="weather-table__td">Humidity</td>
-                        <td className="weather-table__td">
-                          {obj.main.humidity} %
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="weather-table__td">Sunrise</td>
-                        <td className="weather-table__td">
-                          {format(obj.sys.sunrise * 1000, "HH:mm")}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="weather-table__td">Sunset</td>
-                        <td className="weather-table__td">
-                          {format(obj.sys.sunset * 1000, "HH:mm")}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              )
+              obj.cod === "404" ? null : <Card key={obj.id} obj={obj} />
             )}
       </div>
     );
